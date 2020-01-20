c4c51418a4324572cff42387450803aab364c8d3
Akansha <42759920+Akanshark@users.noreply.github.com>
2020-01-20 13:28:08 +0530

    Update ResultComponent.js

--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -1,17 +1,3 @@
-/*import React, { Component } from "react";
-
-class Result extends Component {
-  render() {
-    let { result } = this.props;
-    return (
-      <div className="result">
-        <p>{result}</p>
-      </div>
-    );
-  }
-}
-
-export default Result;*/
 import React, {Component} from 'react';
 
 class ResultComponent extends Component {
